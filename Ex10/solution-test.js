c94c7e0fa8cf12b8045a6981ad8417803bcd708e
surabhi8 <surabhigupta2442@gmail.com>
2018-01-31 12:19:39 +0530

    Verified  the function which is returning a promise

--- Ex10/solution-test.js
+++ Ex10/solution-test.js
@@ -1,13 +1,17 @@
-const { alwaysThrows, iterate } = require('./solution');
+const { alwaysThrows, iterate, newPromise } = require('./solution');
 
 describe('Tests for checking error handling in promises usiong catch', () => {
   test('Alwaysthrows function throws an error', () => {
-    expect(() => { alwaysThrows(); }).toThrowError();
+    expect(() => { alwaysThrows(); }).toThrow();
   });
   test('Alwaysthrows function throws an error OH NOES', () => {
     expect(() => { alwaysThrows(); }).toThrowError('OH NOES');
   });
   test('Iterate function tested for valid inputs', () => {
     expect(iterate(1)).toBe(2);
+  });
+  test('NewPromise returns a promise', (done) => {
+    expect(newPromise()).toBeInstanceOf(Promise);
+    done();
   });
 });
--- Ex10/solution.js
+++ Ex10/solution.js
@@ -3,23 +3,28 @@ const alwaysThrows = () => {
 };
 
 const iterate = (argument) => {
-  console.log(argument);
+  global.console.log(argument);
   return argument + 1;
 };
 const onReject = (error) => {
-  console.log(error.message);
+  global.console.log(error.message);
 };
 
-Promise.resolve(iterate(1))
-  .then(iterate)
-  .then(iterate)
-  .then(iterate)
-  .then(iterate)
-  .then(alwaysThrows)
-  .then(iterate)
-  .then(iterate)
-  .then(iterate)
-  .then(iterate)
-  .then(iterate)
-  .catch(onReject);
-module.exports = { alwaysThrows, iterate, onReject };
+const newPromise = () => {
+  const promise = Promise.resolve(iterate(1))
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .then(alwaysThrows)
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .catch(onReject);
+  return promise;
+};
+module.exports = {
+  alwaysThrows, iterate, onReject, newPromise,
+};
