0d95bbf15c3947f8b9e4e55dc271f908946ee138
surabhi8 <surabhigupta2442@gmail.com>
2018-01-31 12:06:53 +0530

    Test cases modified

--- Ex10/solution-test.js
+++ Ex10/solution-test.js
@@ -2,7 +2,10 @@ const { alwaysThrows, iterate } = require('./solution');
 
 describe('Tests for checking error handling in promises usiong catch', () => {
   test('Alwaysthrows function throws an error', () => {
-    expect(alwaysThrows()).toBe('OH NOES');
+    expect(() => { alwaysThrows(); }).toThrowError();
+  });
+  test('Alwaysthrows function throws an error OH NOES', () => {
+    expect(() => { alwaysThrows(); }).toThrowError('OH NOES');
   });
   test('Iterate function tested for valid inputs', () => {
     expect(iterate(1)).toBe(2);
--- Ex10/solution.js
+++ Ex10/solution.js
@@ -1,7 +1,25 @@
-const alwaysThrows = () => 'OH NOES';
+const alwaysThrows = () => {
+  throw new Error('OH NOES');
+};
 
 const iterate = (argument) => {
   console.log(argument);
   return argument + 1;
 };
-module.exports = { alwaysThrows, iterate };
+const onReject = (error) => {
+  console.log(error.message);
+};
+
+Promise.resolve(iterate(1))
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(alwaysThrows)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .catch(onReject);
+module.exports = { alwaysThrows, iterate, onReject };
